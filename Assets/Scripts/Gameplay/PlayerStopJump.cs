44c2e75f6684513c3852f8c9c92650d7c69e8e4a
Alexandre Oliveira <alexandresousaa70@gmail.com>
2025-11-15 18:09:45 -0300

    README e Comentarios desnecessarios no codigo

--- Assets/Scripts/Gameplay/PlayerStopJump.cs
+++ Assets/Scripts/Gameplay/PlayerStopJump.cs
@@ -4,7 +4,6 @@ using Platformer.Mechanics;
 namespace Platformer.Gameplay
 {
     /// <summary>
-    /// Fired when the Jump Input is deactivated by the user, cancelling the upward velocity of the jump.
     /// </summary>
     /// <typeparam name="PlayerStopJump"></typeparam>
     public class PlayerStopJump : Simulation.Event<PlayerStopJump>
